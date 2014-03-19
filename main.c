cozybit/wcn36xx-xz

wcn36xx: fix the mesh STA for rejoin
The firmware has triggered the QoS NULL frame to monitor the links
amongst peer mesh STAs. Once one of the peer mesh STAs is leaving or
out of the range and no ACK received from the peer mesh STA, the
firmware will automatically delete the peer mesh STA.

After deleting the mesh STA and adding the mesh STA back with the HAL
command returning no error, the STA still won't work. Unicast data
frames is not able to transmit out from the mesh STA.

Now, re-insert the inactivite mesh STA with WCN36XX_HAL_CONFIG_STA_REQ
command with action field set to update seems to solve the problem.

Signed-off-by: Chun-Yeow Yeoh <yeohchunyeow@cozybit.com>
Signed-off-by: Jason Mobarak <jam@cozybit.com>
Signed-off-by: Javier Lopez <jlopex@cozybit.com>
Signed-off-by: Javier Cardona <javier@cozybit.com>

main.c
@@ -239,7 +239,9 @@ static int wcn36xx_start(struct ieee80211_hw *hw)
 		if (ret)
 			wcn36xx_warn("Exchange feature caps failed\n");
 	}
+
 	INIT_LIST_HEAD(&wcn->vif_list);
+
 	return 0;
 
 out_smd_stop:
@@ -616,7 +618,7 @@ static void wcn36xx_bss_info_changed(struct ieee80211_hw *hw,
 			 * config_sta must be called from  because this is the
 			 * place where AID is available.
 			 */
-			wcn36xx_smd_config_sta(wcn, vif, sta);
+			wcn36xx_smd_config_sta(wcn, vif, sta, 0);
 			rcu_read_unlock();
 		} else {
 			wcn36xx_dbg(WCN36XX_DBG_MAC,
@@ -722,6 +724,9 @@ static int wcn36xx_add_interface(struct ieee80211_hw *hw,
 		return -EOPNOTSUPP;
 	}
 
+	INIT_LIST_HEAD(&vif_priv->sta_list);
+	spin_lock_init(&vif_priv->sta_list_spinlock);
+
 	list_add(&vif_priv->list, &wcn->vif_list);
 	wcn36xx_smd_add_sta_self(wcn, vif);
 
@@ -734,11 +739,23 @@ static int wcn36xx_sta_add(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 	struct wcn36xx *wcn = hw->priv;
 	struct wcn36xx_vif *vif_priv = (struct wcn36xx_vif *)vif->drv_priv;
 	struct wcn36xx_sta *sta_priv = (struct wcn36xx_sta *)sta->drv_priv;
+	struct wcn36xx_sta *sta_priv_existing = NULL;
 	wcn36xx_dbg(WCN36XX_DBG_MAC, "mac sta add vif %p sta %pM\n",
 		    vif, sta->addr);
 
 	vif_priv->sta = sta_priv;
 	sta_priv->vif = vif_priv;
+
+	spin_lock(&vif_priv->sta_list_spinlock);
+	sta_priv_existing = wcn36xx_find_sta(vif_priv, sta->addr);
+
+	if (sta_priv_existing)
+		sta_priv = sta_priv_existing;
+	else
+		list_add(&sta_priv->list, &vif_priv->sta_list);
+
+	spin_unlock(&vif_priv->sta_list_spinlock);
+
 	/*
 	 * For STA mode HW will be configured on BSS_CHANGED_ASSOC because
 	 * at this stage AID is not available yet.
@@ -746,8 +763,12 @@ static int wcn36xx_sta_add(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 	if (NL80211_IFTYPE_STATION != vif->type) {
 		wcn36xx_update_allowed_rates(sta, WCN36XX_BAND(wcn));
 		sta_priv->aid = sta->aid;
-		wcn36xx_smd_config_sta(wcn, vif, sta);
+		if (sta_priv_existing)
+			wcn36xx_smd_config_sta(wcn, vif, sta, 1);
+		else
+			wcn36xx_smd_config_sta(wcn, vif, sta, 0);
 	}
+
 	return 0;
 }
 
@@ -758,13 +779,30 @@ static int wcn36xx_sta_remove(struct ieee80211_hw *hw,
 	struct wcn36xx *wcn = hw->priv;
 	struct wcn36xx_vif *vif_priv = (struct wcn36xx_vif *)vif->drv_priv;
 	struct wcn36xx_sta *sta_priv = (struct wcn36xx_sta *)sta->drv_priv;
+	struct wcn36xx_sta *sta_priv_existing = NULL;
 
 	wcn36xx_dbg(WCN36XX_DBG_MAC, "mac sta remove vif %p sta %pM index %d\n",
 		    vif, sta->addr, sta_priv->sta_index);
 
-	wcn36xx_smd_delete_sta(wcn, sta_priv->sta_index);
+	spin_lock(&vif_priv->sta_list_spinlock);
+	sta_priv_existing = wcn36xx_find_sta(vif_priv, sta->addr);
+
+	if (sta_priv_existing)
+		list_del(&sta_priv_existing->list);
+
+	spin_unlock(&vif_priv->sta_list_spinlock);
+
 	vif_priv->sta = NULL;
-	sta_priv->vif = NULL;
+
+	if (sta_priv_existing) {
+		sta_priv_existing->vif = NULL;
+		wcn36xx_smd_delete_sta(wcn, sta_priv_existing->sta_index);
+	}
+	else {
+		sta_priv->vif = NULL;
+		wcn36xx_smd_delete_sta(wcn, sta_priv_existing->sta_index);
+	}
+
 	return 0;
 }
 

silverjam/wcn36xx

main.c
@@ -673,7 +673,7 @@ static void wcn36xx_bss_info_changed(struct ieee80211_hw *hw,
 			 * config_sta must be called from  because this is the
 			 * place where AID is available.
 			 */
-			wcn36xx_smd_config_sta(wcn, vif, sta);
+			wcn36xx_smd_config_sta(wcn, vif, sta, 0);
 			rcu_read_unlock();
 		} else {
 			wcn36xx_dbg(WCN36XX_DBG_MAC,
@@ -777,6 +777,9 @@ static int wcn36xx_add_interface(struct ieee80211_hw *hw,
 		return -EOPNOTSUPP;
 	}
 
+	INIT_LIST_HEAD(&vif_priv->sta_list);
+	spin_lock_init(&vif_priv->sta_list_spinlock);
+
 	list_add(&vif_priv->list, &wcn->vif_list);
 	wcn36xx_smd_add_sta_self(wcn, vif);
 
@@ -789,10 +792,22 @@ static int wcn36xx_sta_add(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 	struct wcn36xx *wcn = hw->priv;
 	struct wcn36xx_vif *vif_priv = wcn36xx_vif_to_priv(vif);
 	struct wcn36xx_sta *sta_priv = wcn36xx_sta_to_priv(sta);
+	struct wcn36xx_sta *sta_priv_existing = NULL;
 	wcn36xx_dbg(WCN36XX_DBG_MAC, "mac sta add vif %p sta %pM\n",
 		    vif, sta->addr);
 
 	sta_priv->vif = vif_priv;
+
+	spin_lock(&vif_priv->sta_list_spinlock);
+	sta_priv_existing = wcn36xx_find_sta(vif_priv, sta->addr);
+
+	if (sta_priv_existing)
+		sta_priv = sta_priv_existing;
+	else
+		list_add(&sta_priv->list, &vif_priv->sta_list);
+
+	spin_unlock(&vif_priv->sta_list_spinlock);
+
 	/*
 	 * For STA mode HW will be configured on BSS_CHANGED_ASSOC because
 	 * at this stage AID is not available yet.
@@ -800,7 +815,7 @@ static int wcn36xx_sta_add(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 	if (NL80211_IFTYPE_STATION != vif->type) {
 		wcn36xx_update_allowed_rates(sta, WCN36XX_BAND(wcn));
 		sta_priv->aid = sta->aid;
-		wcn36xx_smd_config_sta(wcn, vif, sta);
+		wcn36xx_smd_config_sta(wcn, vif, sta, !!sta_priv_existing);
 	}
 	return 0;
 }
@@ -811,12 +826,32 @@ static int wcn36xx_sta_remove(struct ieee80211_hw *hw,
 {
 	struct wcn36xx *wcn = hw->priv;
 	struct wcn36xx_sta *sta_priv = wcn36xx_sta_to_priv(sta);
+	struct wcn36xx_vif *vif_priv = wcn36xx_vif_to_priv(vif);
+	struct wcn36xx_sta *sta_priv_existing = NULL;
+	u16 sta_index;
 
 	wcn36xx_dbg(WCN36XX_DBG_MAC, "mac sta remove vif %p sta %pM index %d\n",
 		    vif, sta->addr, sta_priv->sta_index);
 
-	wcn36xx_smd_delete_sta(wcn, sta_priv->sta_index);
-	sta_priv->vif = NULL;
+	spin_lock(&vif_priv->sta_list_spinlock);
+	sta_priv_existing = wcn36xx_find_sta(vif_priv, sta->addr);
+
+	if (sta_priv_existing)
+	{
+		list_del(&sta_priv_existing->list);
+		sta_priv_existing->vif = NULL;
+		sta_index = sta_priv_existing->sta_index;
+	}
+
+	spin_unlock(&vif_priv->sta_list_spinlock);
+
+	if (sta_priv_existing) {
+		wcn36xx_smd_delete_sta(wcn, sta_index);
+	} else {
+		sta_priv->vif = NULL;
+		wcn36xx_smd_delete_sta(wcn, sta_priv->sta_index);
+	}
+
 	return 0;
 }
 